3139aaece910e7c01bee0957e6db2dca3ae91e67
hegifebrianto <hegifebrianto@gmail.com>
2017-12-19 22:27:20 +0700

    revisi day2 ex2.js

--- Senin - Dunia JavaScript/exercise-2.js
+++ Senin - Dunia JavaScript/exercise-2.js
@@ -2,18 +2,6 @@
 var nama = 'Mikael';
 var peran = '';
 
-//Contoh input 2
-var nama = 'Nina';
-var peran = 'Ksatria';
-
-//Contoh input 3
-var nama = 'Danu';
-var peran = 'Tabib';
-
-//Contoh input 4
-var nama = 'Zero';
-var peran = 'Penyihir';
-
 
 
 
